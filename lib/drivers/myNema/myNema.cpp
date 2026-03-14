72cc2bd8ca6afe42f9e0b6b669113cd321fed178
EEGAA <erick.grimaldo5531@alumnos.udg.mx>
2026-03-14 12:31:31 -0600

    complete la clase myNEMA NEMAgen, ahora ya esta completa NO tienen ningun metodo que por si solo mueva el motor la clase se encarga de manipular el driver y definir cantidad de pasos, sentido de giro y futuramente el intervalo entre pasos
    El movimiento el cual normalmente se hacia dentro de
    un ciclo for y usando delay para swichar el estado del pin
    step se realiza ahora usando tareas de freeRTOS, siendo una tarea
    la encargada en su loop de swichear el estado de step

--- lib/drivers/myNema/myNema.cpp
+++ lib/drivers/myNema/myNema.cpp
@@ -7,6 +7,8 @@ NEMAgen::NEMAgen(uint8_t dir_, uint8_t step_, uint8_t enable_){
     pinMode(dir, OUTPUT);
     pinMode(step, OUTPUT);
     pinMode(enable, OUTPUT);
+    resetContStep();
+    setTotalStep(1);
     setEnable(false);
 }
 
@@ -20,6 +22,29 @@ void NEMAgen::setEnable(bool bnd){
     }
 }
 
-void NEMAgen::printDir(bool bnd){
+void NEMAgen::setDir(bool bnd){
     digitalWrite(dir, bnd);
+}
+
+void NEMAgen::setStep(bool bnd){
+    digitalWrite(step, bnd);
+}
+uint32_t NEMAgen::getContStep(){
+    return contStep;
+}
+void NEMAgen::resetContStep(){
+    this->contStep = 0;
+}
+bool NEMAgen::sumContStep(){
+    if(contStep == totalStep)
+        return false;
+    else
+        contStep++;
+    return true;
+}
+void NEMAgen::setTotalStep(uint32_t x){
+    this->totalStep = x;
+}
+uint32_t NEMAgen::getTotalStep(){
+    return totalStep;
 }
--- lib/drivers/myNema/myNema.hpp
+++ lib/drivers/myNema/myNema.hpp
@@ -4,9 +4,15 @@
 class NEMAgen{
 private:
     uint8_t dir, step, enable;
+    uint32_t contStep, totalStep;
 public:
     NEMAgen(uint8_t dir_, uint8_t step_, uint8_t enable_);
     void setEnable(bool bnd);
-    void printDir(bool bnd);
-
+    void setDir(bool bnd);
+    void setStep(bool bnd);
+    uint32_t getContStep();
+    void resetContStep();
+    bool sumContStep();
+    void setTotalStep(uint32_t x);
+    uint32_t getTotalStep();
 };
